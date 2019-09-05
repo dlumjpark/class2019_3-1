5b81248858732636c285d6ac5aad5c2e54c0d26b
dlumjpark <mjpark@daelim.ac.kr>
2019-09-05 16:31:24 +0900

    코드 변경

--- myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
+++ myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
@@ -1,5 +1,25 @@
 package kr.ac.daelim.uml;
 
-public class Marine {
+public class Marine extends Unit {
+	
+	public String name;
+	public int health;
+	public int attackPower;
+	
+	public void move()
+	{
+		System.out.println("마린이 움직인다");
+	}
+	
+	public void attack()
+	{
+		System.out.println("마린이 공격한다");
+	}
+	
+	public void underAttack(Charactor charactor)
+	{
+		System.out.println("마린이 공격받는다");		
+	}
+	
 
 }
--- myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
+++ myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
@@ -1,5 +1,19 @@
 package kr.ac.daelim.uml;
 
-public class Medic {
+public class Medic extends Unit{
 
+	public void move()
+	{
+		System.out.println("메딕이 움직인다");
+	}
+	
+	public void underAttack(Charactor charactor)
+	{
+		System.out.println("메딕이 공격받는다");
+	}
+	
+	public void healing(Charactor charactor)
+	{
+		System.out.println("메딕이 치료한다");
+	}
 }
